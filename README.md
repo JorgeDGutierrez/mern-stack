20001c432357a585f38d5b5ec0ff93b4903865c5
Jorge Gutierrez <jorge.gutierrezl@outlook.com>
2021-08-09 00:24:03 -0500

    final

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Aplicacion desarrollada con react, express y mongodb
+
+## ejecutar npm run webpack y npm run dev
---
+++ src/database.js
@@ -0,0 +1,11 @@
+const { connect } = require('mongodb');
+const mongoose = require('mongoose');
+
+
+const URI = 'mongodb://localhost/mern-tasks'
+mongoose.connect(URI,{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(db => console.log('DB is connected'))
+    .catch(err => console.log(err))
+
+
+    module.exports = mongoose;
